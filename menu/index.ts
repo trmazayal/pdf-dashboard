864be5b407e49c3a359a6e5f4fa39e089f5e92a8
Tara Mazaya Lababan <tara.mazaya@ui.ac.id>
2024-08-14 10:32:03 +0700

    refactor: Update menu icons and labels

--- menu/index.ts
+++ menu/index.ts
@@ -1,84 +1,83 @@
-import IconOverview from '@privyid/persona-icon/vue/dashboard/20.vue'
-import IconUserRole from '@privyid/persona-icon/vue/user-groups/20.vue'
-import IconEnterpriseSeal from '@privyid/persona-icon/vue/lock/20.vue'
-import IconReminder from '@privyid/persona-icon/vue/time/20.vue'
-import IconEmailLogo from '@privyid/persona-icon/vue/envelope/20.vue'
-import IconEmployeeHandover from '@privyid/persona-icon/vue/collaboration/20.vue'
-import IconCategoryDocument from '@privyid/persona-icon/vue/folder/20.vue'
-import IconBilling from '@privyid/persona-icon/vue/document-filled/20.vue'
-import IconPaymentHistory from '@privyid/persona-icon/vue/receipt-history/20.vue'
-import IconReport from '@privyid/persona-icon/vue/report-statistics/20.vue'
+import IconAction from '@privyid/persona-icon/vue/need-action/20.vue'
+import IconInbox from '@privyid/persona-icon/vue/inbox/20.vue'
+import IconDocumentExport from '@privyid/persona-icon/vue/document-export/20.vue'
+import IconDocumentDraft from '@privyid/persona-icon/vue/document-draft/20.vue'
+import IconDocumentCheck from '@privyid/persona-icon/vue/document-check/20.vue'
+import IconArchiveBox from '@privyid/persona-icon/vue/archive-box/20.vue'
+import IconDocumentTemplate from '@privyid/persona-icon/vue/template/20.vue'
+import IconGroupContact from '@privyid/persona-icon/vue/group-contact/20.vue'
+import IconCollaboration from '@privyid/persona-icon/vue/collaboration/20.vue'
+import IconDataStructure from '@privyid/persona-icon/vue/data-structure/20.vue'
 
 import { defineMenu } from '@privyid/persona/core'
 
 export default defineMenu([
   {
-    title: 'General',
     items: [
       {
-        name : 'overview',
-        label: 'Overview',
-        url  : '/',
-        icon : IconOverview,
-      },
-      {
-        name : 'user-and-role',
-        label: 'User and role',
-        url  : '/user-role',
-        icon : IconUserRole,
-      },
-      {
-        name : 'enterprise-seal',
-        label: 'Enterprise seal',
-        url  : '/enterprise',
-        icon : IconEnterpriseSeal,
-      },
-      {
-        name : 'reminder',
-        label: 'Reminder',
-        url  : '/reminder',
-        icon : IconReminder,
-      },
-      {
-        name : 'email-logo',
-        label: 'Email logo',
-        url  : '/email',
-        icon : IconEmailLogo,
-      },
-      {
-        name : 'employee-handover',
-        label: 'Employee handover',
-        url  : '/employee',
-        icon : IconEmployeeHandover,
-      },
-      {
-        name : 'category-document',
-        label: 'Category document',
-        url  : '/category',
-        icon : IconCategoryDocument,
+        name : 'dashboard',
+        label: 'Actions required',
+        url  : '/dashboard',
+        icon : IconAction,
       },
     ],
   },
   {
-    title: 'Other',
+    maxLength: 5,
     items: [
       {
-        name : 'billing',
-        label: 'Billing',
-        url  : '/billing',
-        icon : IconBilling,
+        name : 'overview',
+        label: 'Inbox',
+        url  : '/',
+        icon : IconInbox,
       },
       {
-        name : 'payment-history',
-        label: 'Payment history',
-        url  : '/payment-history',
-        icon : IconPaymentHistory,
+        name : 'user-and-role',
+        label: 'Sent',
+        url  : '/user-role',
+        icon : IconDocumentExport,
       },
       {
-        name : 'report',
-        label: 'Report',
-        url  : '/report',
-        icon : IconReport,
+        name : 'enterprise-seal',
+        label: 'Draft',
+        url  : '/enterprise',
+        icon : IconDocumentDraft,
+      },
+      {
+        name : 'reminder',
+        label: 'Completed',
+        url  : '/reminder',
+        icon : IconDocumentCheck,
+      },
+      {
+        name : 'email-logo',
+        label: 'Archieve',
+        url  : '/email',
+        icon : IconArchiveBox,
+      },
+      {
+        name : 'email-logo',
+        label: 'Document Template',
+        url  : '/document-template',
+        icon : IconDocumentTemplate,
+      },
+      {
+        name : 'email-logo',
+        label: 'Grooups',
+        url  : '/email',
+        icon : IconGroupContact,
+      },
+      {
+        name : 'email-logo',
+        label: 'Handover',
+        url  : '/email',
+        icon : IconCollaboration,
+      },
+      {
+        name : 'email-logo',
+        label: 'Employee',
+        url  : '/email',
+        icon : IconDataStructure,
       },
     ],
   },
